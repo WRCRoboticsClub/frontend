533adeda53cdcc9e814d2e7ab428824f6ba4ba55
Sunilshah-7 <sunilshah2416@gmail.com>
2021-10-28 22:08:06 +0545

    Social Media Icons

--- components/Header/header.js
+++ components/Header/header.js
@@ -3,19 +3,39 @@ import clubLogo from "../../assets/Logo.png";
 import styles from "./Header.module.css";
 import { navLinks } from "./navLinks";
 import Link from "next/link";
+import { SocialIcon } from "react-social-icons";
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <div className={styles.leftContainer}>
         <Image src={clubLogo} width={75} height={75} responsive="true" />
-        <h1 className={styles.title}>Robotics Club, WRC</h1>
+        <div className={styles.titleAndSocial}>
+          <h1 className={styles.title}>Robotics Club, WRC</h1>
+          <div>
+            <SocialIcon
+              url="https://www.facebook.com/wrcrobo"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+            {"  "}
+            <SocialIcon
+             icon="Gmail"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          </div>
+        </div>
       </div>
 
       <nav className={styles.navbar}>
         {navLinks.map((link, index) => (
-          <ul  key={index} className={styles.rightContainer}>
-            <Link href={link.path} >
+          <ul key={index} className={styles.rightContainer}>
+            <Link href={link.path}>
               <li key={index}>{link.name}</li>
             </Link>
           </ul>
