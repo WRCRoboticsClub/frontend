7b28ab80cfcdde5596f5e5745743cb8187887bbc
Sunilshah-7 <sunilshah2416@gmail.com>
2021-10-28 18:29:24 +0545

    second initial

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,3 +1,11 @@
+import Image from "next/image";
+import clubLogo from "../../assets/club_logo.png";
+import "./Header.module.css";
 export default function Header() {
-  return <div> The Header Tab</div>;
+  return (
+    <div className="container">
+      <Image src={clubLogo} width={100} height={100} responsive="true" />
+      <h1>Robotics Club, WRC</h1>
+    </div>
+  );
 }
