937fa93a0daecf2e5a61820bc5323eba3da5c989
Sunilshah-7 <sunilshah2416@gmail.com>
2021-10-31 20:01:21 +0545

    Committee page added

--- src/pages/committee.js
+++ src/pages/committee.js
@@ -1,3 +1,75 @@
+/** @jsxImportSource @theme-ui/core */
+import { jsx } from "theme-ui";
+import { Container, Grid } from "theme-ui";
+import SectionHeader from "../components/section-header";
+import TeamCard from "../components/team-card";
+import {data} from "../components/header/committee.data";
+
 export default function Committee() {
-  return <h1>Committee</h1>;
+  return (
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <SectionHeader
+          slogan="Meet Our Incredible team"
+          title="The most qualified and professional"
+        />
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <TeamCard
+              key={item.id}
+              src={item.imgSrc.src}
+              title={item.title}
+              altText={item.altText}
+              designation={item.designation}
+              social={item.socialProfile}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
+  );
 }
+
+const styles = {
+  banner: {
+    pt: ["140px", "145px", "155px", "170px", null, null, "180px", "215px"],
+    pb: [2, null, 0, null, 2, 0, null, 5],
+    position: "relative",
+    zIndex: 2,
+
+    container: {
+      minHeight: "inherit",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    contentBox: {
+      width: ["100%", "90%", "535px", null, "57%", "60%", "68%", "60%"],
+      mx: "auto",
+      textAlign: "center",
+      mb: ["40px", null, null, null, null, 7],
+    },
+    imageBox: {
+      justifyContent: "center",
+      textAlign: "center",
+      display: "inline-flex",
+      mb: [0, null, -6, null, null, "-40px", null, -3],
+      img: {
+        position: "relative",
+        height: [200, "auto"],
+        // width: [200, "auto"],
+      },
+    },
+  },
+  grid: {
+    mt: [0, null, -6, null, -4],
+    gridGap: ["35px 0px", null, 0, null, null, "30px 35px"],
+    gridTemplateColumns: [
+      "repeat(2,1fr)",
+      null,
+      "repeat(2,1fr)",
+      null,
+      "repeat(3,1fr)",
+    ],
+  },
+};
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,4 +1,42 @@
 export default function Events() {
-    return <h1>Events</h1>;
-  }
-  
+  return (
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <h1>Events</h1>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  banner: {
+    pt: ["140px", "145px", "155px", "170px", null, null, "180px", "215px"],
+    pb: [2, null, 0, null, 2, 0, null, 5],
+    position: "relative",
+    zIndex: 2,
+
+    container: {
+      minHeight: "inherit",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+    contentBox: {
+      width: ["100%", "90%", "535px", null, "57%", "60%", "68%", "60%"],
+      mx: "auto",
+      textAlign: "center",
+      mb: ["40px", null, null, null, null, 7],
+    },
+    imageBox: {
+      justifyContent: "center",
+      textAlign: "center",
+      display: "inline-flex",
+      mb: [0, null, -6, null, null, "-40px", null, -3],
+      img: {
+        position: "relative",
+        height: [200, "auto"],
+        // width: [200, "auto"],
+      },
+    },
+  },
+};
